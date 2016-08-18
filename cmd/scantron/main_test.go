4cda849e2eda6bd54a9514154ce832c0d24af461
Kris Hicks <khicks@pivotal.io>
2016-08-18 12:21:35 -0700

    main_test pretends to be a real test
    This is just testing that it builds correctly, which is done in the suite test.

--- cmd/scantron/main_test.go
+++ cmd/scantron/main_test.go
@@ -2,9 +2,11 @@ package main_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Main", func() {
-
+	It("builds", func() {
+		Expect(true).To(Equal(true))
+	})
 })
