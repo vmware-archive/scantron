b383d3e7a164e9a7601e71f62bf2e7ede86a8c79
Mark Fussell <mfussell@pivotal.io>
2016-09-16 15:20:28 -0700

    actually check we can compile the cli
    Signed-off-by: Christopher Brown <cbrown@pivotal.io>

--- cmd/scantron/main_test.go
+++ cmd/scantron/main_test.go
@@ -3,10 +3,12 @@ package main_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Main", func() {
 	It("builds", func() {
-		Expect(true).To(Equal(true))
+		_, err := gexec.Build("github.com/pivotal-cf/scantron/cmd/scantron")
+		Expect(err).NotTo(HaveOccurred())
 	})
 })
