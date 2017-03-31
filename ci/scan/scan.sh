8f7dd40a2aaca511ae6fb500a95b17c81263470c
Christopher Brown <cbrown@pivotal.io>
2017-03-31 09:58:45 -0700

    add scanning concourse task

---
+++ ci/scan/scan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+chmod +x ./scantron-binary/scantron
+
+NMAP_RESULTS="results.xml"
+
+# perform nmap scan
+nmap -oX "$NMAP_RESULTS" --script ssl-enum-ciphers -sV -p - "$NMAP_RANGE"
+
+# perform scantron scan
+./scantron-binary/scantron bosh-scan \
+  --nmap-results "$NMAP_RESULTS" \
+  --director-url "$BOSH_ADDRESS" \
+  --bosh-deployment "$BOSH_DEPLOYMENT" \
+  --uaa-client "$BOSH_CLIENT_ID" \
+  --uaa-client-secret "$BOSH_CLIENT_SECRET"
